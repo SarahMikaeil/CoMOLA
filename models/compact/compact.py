248341a60f75f38020060a7c2c5b507f19401694
SarahMikaeil <sarahemadmikaeil@alexu.edu.eg>
2023-11-02 23:28:27 +0200

    Added Spatial Compactness, Compatibility and Economic Objectives External Models

---
+++ models/compact/compact.py
@@ -0,0 +1,24 @@
+import numpy as np
+def getsumofneighbors(matrix, i, j, value):
+    if value < 0:
+        return 0
+    region = matrix[max(0, i-1) : i+2,
+                    max(0, j-1) : j+2]
+    region = region[region == value]
+    return region.size - 1
+
+inputMatrix = np.loadtxt(fname = "E:\Msc\CoMOLA-master\models\compact\map.asc", skiprows=6)
+rows, columns = inputMatrix.shape
+results = 0
+
+for i in range(rows):
+    for j in range(columns):
+        cellValue = inputMatrix[i][j]
+        result = getsumofneighbors(inputMatrix, i, j, cellValue)
+        print(result)
+        results += result
+
+print("resultsssssssssssssssss", results)
+f = open("E:\Msc\CoMOLA-master\models\compact\compact_output.csv", "w")
+f.write(str(results))
+f.close
---
+++ models/economy/economy.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+inputMatrix = np.loadtxt(fname = "E:\Msc\CoMOLA-master\models\economy\map.asc", skiprows=6)
+rows, columns = inputMatrix.shape
+economy_landuses = np.array([3, 7])
+results = 0
+
+for i in range(rows):
+    for j in range(columns):
+        cellValue = inputMatrix[i][j]
+        if cellValue in economy_landuses:
+          results += pow(25, 2)
+print("resultsssssssssssssssss", results)
+f = open("E:\Msc\CoMOLA-master\models\economy\economy_output.csv", "w")
+f.write(str(results))
+f.close
